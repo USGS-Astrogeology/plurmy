42708c15422689616047a044e9e8d1cb2719f868
jay <jlaura@usgs.gov>
2018-09-02 14:15:28 -0700

    Adds setup.py for install

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name='plurmy',
+    version='0.1.0',
+    long_description='',
+    packages=find_packages(),
+    include_package_data=True
+)
